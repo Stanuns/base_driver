d2c7921c6c50515d9444250d05f8dd2d91a26b25
wei.sun <sunweijm.sun@gmail.com>
2025-03-20 09:47:46 +0800

    增加 odom read and publisher

--- base_driver/hm_base_driver.py
+++ base_driver/hm_base_driver.py
@@ -4,12 +4,19 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import UInt8
+from nav_msgs.msg import Odometry
 import serial
 import struct
 from robot_interfaces.msg import HMAutoDockState
 from robot_interfaces.msg import HMAutoDockTrigger
 import time
 import traceback
+from tf_transformations import quaternion_from_euler
+
+class BaseSerialError(Exception):
+    def __init__(self, message=""):
+        self.message = message
+        super(BaseSerialError,self).__init__()
 
 class HmBaseNode(Node):
     def __init__(self):
@@ -20,7 +27,7 @@ class HmBaseNode(Node):
 
         # 初始化下位机串口连接
         self.ser_base = serial.Serial(
-            port='/dev/hm_base',
+            port='/dev/ttyUSB0',
             baudrate=115200,
             bytesize=serial.EIGHTBITS,
             parity=serial.PARITY_NONE,
@@ -46,7 +53,7 @@ class HmBaseNode(Node):
 
         self.subscription = self.create_subscription(
             Twist,
-            '/hm_cmd_vel',
+            '/cmd_vel',
             self.handle_hm_cmd_vel,
             10
         )
@@ -57,10 +64,17 @@ class HmBaseNode(Node):
             10
         )
 
+        # publisher for /odom
+        self.odom_publisher = self.create_publisher(
+            Odometry,
+            '/odom',
+            10
+        )
+
         # New publisher for /hm_dock_state
         self.dock_state_publisher = self.create_publisher(
-            UInt8,
-            '/hm_dock_state',
+            HMAutoDockState,
+            '/hm_auto_dock_state',
             10
         )
 
@@ -73,7 +87,7 @@ class HmBaseNode(Node):
 
         self.get_logger().info("Node initialized")
 
-    #处理cmd_vel
+    # 处理hm_cmd_vel
     def handle_hm_cmd_vel(self, msg):
         """处理速度命令的回调函数"""
         try:
@@ -94,7 +108,7 @@ class HmBaseNode(Node):
         except ValueError as e:
             self.get_logger().warn(f"Invalid value: {e}")
     
-    #处理hm_auto_dock
+    # 处理hm_auto_dock
     def handle_hm_auto_dock(self, msg):
         """处理"""
         try:
@@ -105,7 +119,7 @@ class HmBaseNode(Node):
         except ValueError as e:
             self.get_logger().warn(f"Invalid value: {e}")
 
-    ####################################控制信息发送至底盘下位机#############################################
+    ### 控制信息发送至底盘下位机
     def send_speed_approximately(self, action_value):
         """构建并发送数据帧"""
         try:
@@ -192,46 +206,173 @@ class HmBaseNode(Node):
             traceback.print_exc()
             return None 
     
-    ####################################读取底盘下位机返回信息##############################################
+    ### 读取底盘下位机返回信息
     def read_base_serial_data(self):
         """读取串口数据并处理"""
         while rclpy.ok():
-            # self.get_logger().info(f"----read_base_serial_data----")
-            if self.ser_base.in_waiting > 0:
-                # 读取帧头
-                header = self.ser_base.read(2)
+            # # self.get_logger().info(f"----read_base_serial_data----")
+            # if self.ser_base.in_waiting > 0:
+            #     # 读取帧头
+            #     header = self.ser_base.read(2)
 
-                # 读取回充状态
-                if header == b'\xAA\x55':
-                    self.get_logger().info(f"--------------read_base_serial_data--Have read header----------------")
-                    # 读取帧长
-                    frame_length = self.ser_base.read(2)
-                    if frame_length == b'\x00\x04':
-                        # 读取命令码、流水号、系列编号、返回结果
-                        data = self.ser_base.read(4)
-                        if len(data) == 4:
-                            command_code, sequence_number, series_number, result = struct.unpack('BBBB', data)
-                            if command_code == 0x27 and sequence_number == 0x00 and series_number == 0x02:
-                                # 读取帧尾
-                                footer = self.ser_base.read(1)
-                                if footer == b'\x88':
-                                    # 发布返回结果到 /hm_dock_state
-                                    msg = UInt8()
-                                    msg.data = result
-                                    self.dock_state_publisher.publish(msg)
-                                    self.get_logger().info(f"Received dock state: {result}")
-                                else:
-                                    self.get_logger().warn("Invalid frame footer")
-                            else:
-                                self.get_logger().warn("Invalid command code or series number")
-                        else:
-                            self.get_logger().warn("Incomplete data frame")
-                    else:
-                        self.get_logger().warn("Invalid frame length")
-                else:
-                    self.get_logger().warn("Invalid frame header")
+            #     ## 读取回充状态
+            #     if header == b'\xAA\x55':
+            #         self.get_logger().info(f"--------------read_base_serial_data--Have read header----------------")
+            #         # 读取帧长
+            #         frame_length = self.ser_base.read(2)
+            #         if frame_length == b'\x00\x04':
+            #             # 读取命令码、流水号、系列编号、返回结果
+            #             data = self.ser_base.read(4)
+            #             if len(data) == 4:
+            #                 command_code, sequence_number, series_number, result = struct.unpack('BBBB', data)
+            #                 if command_code == 0x27 and sequence_number == 0x00 and series_number == 0x02:
+            #                     # 读取帧尾
+            #                     footer = self.ser_base.read(1)
+            #                     if footer == b'\x88':
+            #                         # 发布返回结果到 /hm_dock_state
+            #                         msg = UInt8()
+            #                         msg.data = result
+            #                         self.dock_state_publisher.publish(msg)
+            #                         self.get_logger().info(f"Received dock state: {result}")
+            #                     else:
+            #                         self.get_logger().warn("Invalid frame footer")
+            #                 else:
+            #                     self.get_logger().warn("Invalid command code or series number")
+            #             else:
+            #                 self.get_logger().warn("Incomplete data frame")
+            #         else:
+            #             self.get_logger().warn("Invalid frame length")
+            #     else:
+            #         self.get_logger().warn("Invalid frame header")
+            data = self.get_base_new_data()
+            if data is None:
+                pass
+                return
+            income_data_type = data.get('type')
+            if income_data_type == 'odom':
+                self.cast_odom(data)
+            elif quaternion_from_euler =='auto_dock':
+                self.cast_dock_state(data)
+            else: 
+                return
 
-    ####################################读取Android pad返回信息##############################################
+    def get_base_new_data(self):
+        try:
+            frame = self.read_base_raw_frame()
+            code = frame.get('code')
+            data_in_frame = frame.get('data')
+            if code == 78: #odom
+                return self._case_odom(data_in_frame)
+            elif code == 27: #回充反馈
+                return self._case_auto_dock(data_in_frame)
+        except BaseSerialError as e:
+            self.get_logger().error(f"Read serial time out. {str(e)}")
+            return None
+
+    def read_base_raw_frame(self):
+        frame = {}
+        e = BaseSerialError()
+        if self.ser_base.in_waiting > 0:
+            header = self.ser_base.read_until(terminator=b'\xAA\x55')
+        
+        if (len(header)>2) and (header != b'\xAA\x55'):
+            e.message = 'Frame corrupted, head can not be reached in time.'
+            raise e
+        
+        zero = self.ser_base.read(1)
+        if zero != b'\x00':
+            e.message = 'Error occur after head reached.'
+            raise e
+
+        len_raw = self.ser_base.read(1)
+        if len(len_raw) != 1:
+            e.message = 'Size byte does not arrive in time.'
+            raise e
+        data_len = ord(len_raw)
+
+        code_raw = self.ser_base.read(1)
+        if len(code_raw) != 1:
+            e.message = 'Command code does not arrive in time.'
+            raise e
+        code = ord(code_raw)  # command code
+
+        sequence_raw = self.ser_base.read(1)
+        if sequence_raw != b'\x00':
+            e.message = 'Sequence number code does not arrive in time.'
+            raise e
+
+        #fix bugs from 华麦
+        if code == 27:
+            data_raw =  self.ser_base.read(data_len-2) #帧长值华麦从底盘上传错误
+        else:
+            data_raw =  self.ser_base.read(data_len-3) #帧长-1命令码-1流水号-1帧尾
+
+        footer = self.ser_base.read(1)
+        if footer != b'\x88':
+            e.message = 'No footer'
+            raise e
+
+        frame['data'] = data_raw  # bytes
+        frame['code'] = code  # int
+        return frame
+
+    def _case_odom(data_raw): 
+        if len(data_raw) != 20:  # refer to the data sheet provided by jinfei
+            e = BaseSerialError()
+            e.message = "Odom data corrupted"
+            raise e
+        data = {'type': 'odom'}
+        x, y, yaw, v, w = struct.unpack('<5f', data_raw)
+        data['x'] = x
+        data['y'] = y
+        data['yaw'] = yaw
+        data['v'] = v
+        data['w'] = w 
+        return data
+    def _case_auto_dock(data_raw):
+        if len(data_raw) != 2:  # refer to the data sheet provided by jinfei
+            e = BaseSerialError()
+            e.message = "dock state data corrupted"
+            raise e
+        data = {'type': 'auto_dock'}
+        series_number, state = struct.unpack('BB', data_raw)
+        data['series_number'] = series_number
+        data['state'] = state
+        return data
+
+
+    def cast_odom(self, data):
+        x = data.get('x')
+        y = data.get('y')
+        yaw = data.get('yaw')
+        v = data.get('v')
+        w = data.get('w')
+        msg = Odometry()
+        msg.header.frame_id = 'odom'
+        msg.child_frame_id = 'base_footprint'
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.pose.pose.position.x = x
+        msg.pose.pose.position.y = y
+        msg.pose.pose.position.z = 0.0
+        quaternion = quaternion_from_euler(0.0, 0.0, yaw)
+        msg.pose.pose.orientation.x = quaternion[0]
+        msg.pose.pose.orientation.y = quaternion[1]
+        msg.pose.pose.orientation.z = quaternion[2]
+        msg.pose.pose.orientation.w = quaternion[3]
+        msg.twist.twist.linear.x = v
+        msg.twist.twist.angular.z = w
+        self.odom_publisher.publish(msg)
+    
+    def cast_dock_state(self, data):
+        msg = HMAutoDockState()
+        msg.header.frame_id = 'auto_dock_state'
+        msg.header.stamp = self.get_clock().now().to_msg()
+        state = data.get('state')
+        msg.state = state
+        self.dock_state_publisher.publish(msg)
+        
+
+    ### 读取Android pad返回信息
     def read_android_serial_data(self):
         """读取android串口数据并处理"""
         while rclpy.ok():
@@ -243,7 +384,7 @@ class HmBaseNode(Node):
                     # 读取帧长
                     frame_length = self.ser_android.read(2)
 
-                    #模糊运动控制
+                    # 模糊运动控制
                     if frame_length == b'\x00\x07':
                         # 读取命令码、流水号、系列编号、动作值、运动时间
                         data = self.ser_android.read(6)
@@ -305,8 +446,8 @@ class HmBaseNode(Node):
         """析构时关闭串口"""
         if hasattr(self, 'ser_base') and self.ser_base.is_open:
             self.ser_base.close()
-        if hasattr(self, 'ser_android') and self.ser_android.is_open:
-            self.ser_android.close()
+        # if hasattr(self, 'ser_android') and self.ser_android.is_open:
+        #     self.ser_android.close()
 
 def main(args=None):
     rclpy.init(args=args)
